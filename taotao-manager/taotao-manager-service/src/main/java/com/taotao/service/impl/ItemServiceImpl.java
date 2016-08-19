23174ee726e396b5eca89e2a2a6f2104ed9a9b9a
jessyon <349283474@qq.com>
2016-08-19 21:44:09 +0800

    淘淘商城第二天，完成主页展示，分页查询

--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.taotao.common.pojo.EasyUIDataGridResult;
 import com.taotao.mapper.TbItemMapper;
 import com.taotao.pojo.TbItem;
 import com.taotao.pojo.TbItemExample;
@@ -35,5 +38,21 @@ public class ItemServiceImpl implements ItemService {
 		}
 		return tbItem;
 	}
-
+	@Override
+	public EasyUIDataGridResult getItemList(int page, int rows) {
+		
+		//分页处理
+		PageHelper.startPage(page, rows);
+		//执行查询
+		TbItemExample example = new TbItemExample();
+		List<TbItem> list = itemMapper.selectByExample(example);
+		//取分页信息
+		PageInfo<TbItem> pageInfo = new PageInfo<>(list);
+		//返回处理结果
+		EasyUIDataGridResult result = new EasyUIDataGridResult();
+		result.setTotal(pageInfo.getTotal());
+		result.setRows(list);
+		
+		return result;
+	}
 }
