b151128cd95b98707d8ed15d767d577c74e93549
jessyon <349283474@qq.com>
2016-08-29 15:47:04 +0800

    内容分类管理，级联删除

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/f7/f0b8be3f946d00161682ed43cc9032b9
@@ -0,0 +1,11 @@
+package com.taotao.service;
+
+import java.util.List;
+
+import com.taotao.common.pojo.EasyUITreeNode;
+
+public interface ContentCategoryService {
+
+	public List<EasyUITreeNode> getContentList(Long parentId);
+	
+}
--- taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ContentCategoryController.java
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ContentCategoryController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.taotao.common.pojo.EasyUITreeNode;
+import com.taotao.common.pojo.TaotaoResult;
 import com.taotao.service.ContentCategoryService;
 
 /**
@@ -27,6 +28,24 @@ public class ContentCategoryController {
 	@ResponseBody
 	public List<EasyUITreeNode> getContentCatList(@RequestParam(value="id",defaultValue="0") Long parentId){
 		return contentCategoryService.getContentList(parentId);
+	}
+	
+	@RequestMapping("/create")
+	@ResponseBody
+	public TaotaoResult createNode(Long parentId,String name){
+		return contentCategoryService.insertCategory(parentId, name);
+	}
+	
+	@RequestMapping("/update")
+	@ResponseBody
+	public void updateNodeName(Long id,String name){
+		contentCategoryService.updateCategoryName(id, name);
+	}
+	
+	@RequestMapping("delete")
+	@ResponseBody
+	public void delete(Long parentId,Long id){
+		contentCategoryService.deleteById(parentId, id);
 	}
 	
 }
