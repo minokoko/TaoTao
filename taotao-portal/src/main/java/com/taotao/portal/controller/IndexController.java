476f12a44a0977578585a6fdc948d13c9decfe10
jessyon <349283474@qq.com>
2016-08-30 14:39:23 +0800

    httpClient测试服务

--- taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
+++ taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
@@ -2,6 +2,8 @@ package com.taotao.portal.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 访问首页controller
@@ -15,4 +17,14 @@ public class IndexController {
 	public String showIndex(){
 		return "index";
 	}
+	
+	@RequestMapping(value="/posttest",method=RequestMethod.POST)
+	@ResponseBody
+	public String postTest(String name ,String pass){
+		System.out.println(name);
+		System.out.println(pass);
+		return "ok";
+	}
+	
+
 }
