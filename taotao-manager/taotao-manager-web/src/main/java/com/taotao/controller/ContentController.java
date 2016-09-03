3ce2143e9f52c848345ad7578155cb6a8b0d5c97
jessyon <349283474@qq.com>
2016-09-03 19:12:20 +0800

    添加redis缓存

--- taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ContentController.java
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ContentController.java
@@ -1,6 +1,7 @@
 package com.taotao.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.taotao.common.pojo.EasyUIDataGridResult;
 import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.common.utils.HttpClientUtil;
 import com.taotao.pojo.TbContent;
 import com.taotao.service.ContentService;
 
@@ -17,6 +19,12 @@ public class ContentController {
 
 	@Autowired
 	private ContentService contentService;
+	
+	@Value("${REST_BASE_URL}")
+	private String REST_BASE_URL;
+	
+	@Value("${REST_CONTENT_SYNC_URL}")
+	private String REST_CONTENT_SYNC_URL;
 	
 	@RequestMapping("/query/list")
 	@ResponseBody
@@ -28,6 +36,8 @@ public class ContentController {
 	@ResponseBody
 	public TaotaoResult insertContent(TbContent content) {
 		TaotaoResult result = contentService.insertContent(content);
+		//调用taotao-rest发布的服务，同步缓存
+		HttpClientUtil.doGet(REST_BASE_URL+REST_CONTENT_SYNC_URL+content.getCategoryId());
 		return result;
 	}
 
--- taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
+++ taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
@@ -1,9 +1,13 @@
 package com.taotao.portal.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.taotao.portal.service.ContentService;
 
 /**
  * 访问首页controller
@@ -13,8 +17,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class IndexController {
 
+	@Autowired
+	private ContentService contentService;
+	
 	@RequestMapping("/index")
-	public String showIndex(){
+	public String showIndex(Model model){
+		//取大广告位内容
+		String json = contentService.getAd1List();
+		model.addAttribute("ad1", json);
 		return "index";
 	}
 	
