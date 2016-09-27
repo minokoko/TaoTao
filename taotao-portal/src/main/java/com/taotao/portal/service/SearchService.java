8feb61b7727ef65bf1241ceefe6adb969bfc0a89
349283474@qq.com <349283474@qq.com>
2016-09-27 10:08:24 +0800

    前台添加搜索功能，并成功测试

---
+++ taotao-portal/src/main/java/com/taotao/portal/service/SearchService.java
@@ -0,0 +1,9 @@
+package com.taotao.portal.service;
+
+import com.taotao.portal.pojo.SearchResult;
+
+public interface SearchService {
+
+	SearchResult search(String keyword, int page, int rows);
+	
+}
